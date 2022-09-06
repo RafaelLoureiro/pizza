5aaa34a87935962ba9104239f5b3b4a8cb7e7321
Rafael Loureiro <rafanetmello@gmail.com>
2022-09-06 15:19:11 -0300

    Pizza slect atribute

--- script.js
+++ script.js
@@ -4,12 +4,20 @@ const cs = (el) => document.querySelectorAll(el);//return array
 pizzaJson.map((item, index) => {
     let pizzaItem = c('.models .pizza-item').cloneNode(true);//trazendo a div(html) (takind tht (div in html))
     //add information in -pizzaitem-
+
+    pizzaItem.setAttribute('data-key', index);//Add data-key (div-pizza--are)
     pizzaItem.querySelector('.pizza-item--img img').src = item.img;
     pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`;//Add (to.price(2) case R$)
     pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
     pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
     pizzaItem.querySelector('a').addEventListener('click', (e) => {
         e.preventDefault();
+        let key = e.target.closest('.pizza-item').getAttribute('data-key');//select atribute key in (pizza-itme)
+        console.log(pizzaJson[key]);
+        c('.pizzaBig img').src = pizzaJson[key].img;
+        c('.pizzaInfo h1').innerHTML = pizzaJson[key].name;
+        c('.pizzaInfo--desc').innerHTML = pizzaJson[key].description;
+
 
         c('.pizzaWindowArea').style.opacity = 0;//add animation tansition
         c('.pizzaWindowArea').style.display = 'flex';//add modal
