73660067f8bafbf84ac06f8ea2428efeee893cd1
RafaelLoureiro <rafnetmello@gmail.com>
2022-09-06 11:22:47 -0300

    add aimation modal

--- script.js
+++ script.js
@@ -8,7 +8,13 @@ pizzaJson.map((item, index) => {
     pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`;//Add (to.price(2) case R$)
     pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
     pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
+    pizzaItem.querySelector('a').addEventListener('click', (e) => {
+        e.preventDefault();
 
+        c('.pizzaWindowArea').style.opacity = 0;//add animation tansition
+        c('.pizzaWindowArea').style.display = 'flex';//add modal
+        setTimeout(() => { c('.pizzaWindowArea').style.opacity = 1; }, 225)//add animation tansition
+    })//aad event tag (a)
 
     c('.pizza-area').append(pizzaItem);//add pizzas
 });
