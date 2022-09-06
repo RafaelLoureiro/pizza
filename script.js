27f8564b985ab11d752067df6ab021a3669aa2fe
RafaelLoureiro <rafnetmello@gmail.com>
2022-09-06 10:42:05 -0300

    add information in -pizzaitem-

--- script.js
+++ script.js
@@ -2,8 +2,13 @@ const c = (el) => document.querySelector(el);//return item
 const cs = (el) => document.querySelectorAll(el);//return array
 
 pizzaJson.map((item, index) => {
-    let pizzaitem = c('.models .pizza-item').cloneNode(true);//trazendo a div(html) (takind tht (div in html))
+    let pizzaItem = c('.models .pizza-item').cloneNode(true);//trazendo a div(html) (takind tht (div in html))
     //add information in -pizzaitem-
+    pizzaItem.querySelector('.pizza-item--img img').src = item.img;
+    pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`;//Add (to.price(2) case R$)
+    pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
+    pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
 
-    c('.pizza-area').append(pizzaitem);//add pizzas
+
+    c('.pizza-area').append(pizzaItem);//add pizzas
 });
